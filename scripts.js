ab55a4279724a93bb7a09c011221b97298c0e84d
TaibaBugammaz <76516938+TaibaBugammaz@users.noreply.github.com>
2021-01-02 21:01:58 +0300

    Done

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// إجابة السؤال الأول
+let myAge=23;
+console.log(myAge);
+
+let myAge5=myAge*5;
+console.log(myAge*5);
+console.log(`حين أبلغ ${myAge5}  سيبدأ زحل باستقبال المبرمجين على سطحه`)
+
+let numberofYears= 2021-1969;
+console.log(`هو عدد الأعوام التي مرت على صعود الإنسان على سطح القمر${numberofYears}`)
